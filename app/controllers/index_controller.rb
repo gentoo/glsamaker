594d6e796ab55205698eb178cb5973f4e164c8ab
Alex Legler <a3li@gentoo.org>
2009-03-15 16:59:24 +0100

    New Authentication stuff
     * Two types of auth: One for production, one for development
     * Error pages

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,18 @@
 class IndexController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :except => :error
   
   def index
-    render :text => "boom"
+    render :text => request.env.inspect
   end
+  
+  def error
+    if params[:type] == "user"
+      render :template => 'index/error-user', :layout => 'notice'
+    elsif params[:type] == "disabled"
+      render :template => 'index/error-disabled', :layout => 'notice'
+    else
+      render :template => 'index/error-system', :layout => 'notice'
+    end
+  end
+  
 end
--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,9 +1,41 @@
 module Authentication
   protected
-  
     def login_required
-      authenticate_or_request_with_http_basic("GLSAMaker") do |username, password|        
-        (username == "user" && password == "foo")
+      # Production authentication via REMOTE_USER
+      if RAILS_ENV == "production"
+        # Autentication system most likely broken
+        if request.env['REMOTE_USER'].nil?
+          redirect_to :controller => 'index', :action => 'error', :type => 'system'
+          return
+        end
+
+        user = User.find_by_login(request.env['REMOTE_USER'])
+
+        # User not known to GLSAMaker
+        if user.nil?
+         redirect_to :controller => 'index', :action => 'error', :type => 'user'
+         return
+        end
+
+        # User is marked as disabled in the DB
+        if user.disabled
+          redirect_to :controller => 'index', :action => 'error', :type => 'disabled'
+          return
+        end
+
+        # Auth is fine now.
+
+      # For all other environments request, HTTP auth by ourselves
+      # The password can be set in config/initializers/glsamaker.rb
+      else
+        authenticate_or_request_with_http_basic("GLSAMaker testing environment") do |username, password|
+          user = User.find_by_login(user)
+
+          return false if user.nil?
+          return false if user.disabled
+
+          password == GLSAMAKER_DEVEL_PASSWORD
+        end
       end
     end
     
