ae55b6d66f294f72f91c3565da4e74bfa4ff0aff
Alex Legler <a3li@gentoo.org>
2009-03-15 19:45:52 +0100

    Authentication now working in more environments
     * works both in Passenger (REMOTE_USER) and via HTTP_AUTHORIZATION
     * Adding config option to override testing auth and require
       production-style auth

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -2,23 +2,28 @@ module Authentication
   protected
     def login_required
       # Production authentication via REMOTE_USER
-      if RAILS_ENV == "production"
+      if RAILS_ENV == "production" or GLSAMAKER_FORCE_PRODUCTION_AUTH
+        # REMOTE_USER should be there in FCGI or Passenger
+        env_user_name = user_name
+      
         # Autentication system most likely broken
-        if request.env['REMOTE_USER'].nil?
+        if env_user_name.nil?
           redirect_to :controller => 'index', :action => 'error', :type => 'system'
           return
         end
 
-        user = User.find_by_login(request.env['REMOTE_USER'])
-
+        user = User.find_by_login(env_user_name)
+        
         # User not known to GLSAMaker
-        if user.nil?
-         redirect_to :controller => 'index', :action => 'error', :type => 'user'
-         return
+        if user == nil
+          logger.warn "Unknown user #{env_user_name} tried to log in from #{request.request_origin}"
+          redirect_to :controller => 'index', :action => 'error', :type => 'user'
+          return
         end
 
         # User is marked as disabled in the DB
         if user.disabled
+          logger.warn "Disabled user #{env_user_name} tried to log in from #{request.request_origin}"
           redirect_to :controller => 'index', :action => 'error', :type => 'disabled'
           return
         end
@@ -30,15 +35,38 @@ module Authentication
       else
         authenticate_or_request_with_http_basic("GLSAMaker testing environment") do |username, password|
           user = User.find_by_login(user)
-
+          
           return false if user.nil?
           return false if user.disabled
 
           password == GLSAMAKER_DEVEL_PASSWORD
         end
       end
+      
+      logger.debug "Environment username: #{env_user_name}"
     end
     
+    # Returns the ActiveRecord object of the currently logged in user
     def current_user
+      User.find_by_login(user_name)
+    end
+    
+  private
+    # Tries to find out the user name used for HTTP auth from two sources
+    def user_name
+      request.env['REMOTE_USER'] || http_authorization_data[0]
+    end
+  
+    def http_authorization_data
+      return nil if request.env['HTTP_AUTHORIZATION'].nil?
+      
+      auth_info = request.env['HTTP_AUTHORIZATION'].split
+      
+      if auth_info[0] == "Basic"
+        auth_info[1].unpack("m*").first.split(/:/, 2)
+      else
+        logger.fatal "Non-Basic HTTP authentication given. I can't process that"
+        raise RuntimeError, "Cannot process this type of HTTP authentication"
+      end
     end
 end
