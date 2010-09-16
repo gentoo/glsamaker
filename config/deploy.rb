36f93e38acc27d2d8199ec5e82dd215a0f1581cc
Alex Legler <a3li@gentoo.org>
2010-09-17 01:26:48 +0200

    Set deploy.rb to lark

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,18 +4,13 @@ set :repository,  "git://git.overlays.gentoo.org/proj/glsamaker.git"
 set :use_sudo, false
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "stingray.a3li.li"                          # Your HTTP server, Apache/etc
-role :app, "stingray.a3li.li"                          # This may be the same as your `Web` server
-role :db,  "stingray.a3li.li", :primary => true # This is where Rails migrations will run
+role :web, "lark.gentoo.org"                          # Your HTTP server, Apache/etc
+role :app, "lark.gentoo.org"                          # This may be the same as your `Web` server
+role :db,  "lark.gentoo.org", :primary => true # This is where Rails migrations will run
 
-set :user, 'alex'
-set :deploy_to, "/home/#{user}/trash/#{application}"
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+#set :user, 'TODO'
+set :deploy_to, "/var/www/glsamaker2.gentoo.org"
 
 namespace :deploy do
   task :start do ; end
