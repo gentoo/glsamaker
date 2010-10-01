82972eb02b1e8970c6fb369523878a4e5a6d0c73
Alex Legler <alex@a3li.li>
2010-10-01 21:26:47 +0200

    Fix wrong foreign key reference table

--- db/migrate/20090410212600_add_revisions_user_id_constraint.rb
+++ db/migrate/20090410212600_add_revisions_user_id_constraint.rb
@@ -1,6 +1,6 @@
 class AddRevisionsUserIdConstraint < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE `revisions` ADD CONSTRAINT revisions_user_userid FOREIGN KEY (user_id) REFERENCES revisions (id)"
+    execute "ALTER TABLE `revisions` ADD CONSTRAINT revisions_user_userid FOREIGN KEY (user_id) REFERENCES users (id)"
   end
 
   def self.down
