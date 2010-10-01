70190d4d0e6bc66cd5b49071793b2fa96eabc8ee
Alex Legler <alex@a3li.li>
2010-10-01 21:57:32 +0200

    Try to fix that module tree problem in dev again..

--- config/initializers/require_libs.rb
+++ config/initializers/require_libs.rb
@@ -11,6 +11,8 @@ unless Rails.env == 'development'
   #require 'lib/bugzilla/bug'
   #require 'lib/bugzilla/history'
   #require 'lib/bugzilla/comment'
+else
+  require 'lib/glsamaker/bugs'
 end
 
 # vim: ts=2 sw=2 et ft=ruby sts=2 tw=72 nospell:
