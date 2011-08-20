19ad0379bce7740facbf4d9ed7afb1fc4b3c60f3
Alex Legler <alex@a3li.li>
2011-08-20 12:35:54 +0200

    ToolsController: file_req_ajax_info is now bugs_ajax_info; Remove old methods

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -14,7 +14,7 @@ class ToolsController < ApplicationController
   layout false
   
   # Provides information for the 'file new request' page
-  def file_req_ajax_info
+  def bugs_ajax_info
     if params[:bugs] == nil
       render :text => "No bug given", :status => 500
       return
@@ -64,62 +64,6 @@ class ToolsController < ApplicationController
     suggestion ||= "(no suggestion available)"
     
     render :json => {"buginfo" => buginfo, "title" => suggestion}
-  end
-  
-  def ajaxbugs
-    bug_ids = Bugzilla::Bug.str2bugIDs(params[:bugs])
-    
-    @bugs = []
-    bug_ids.each do |bug_id|
-      begin
-        @bugs << Bugzilla::Bug.load_from_id(bug_id.to_i)
-      rescue Exception => e
-        @bugs << "Ignoring #{CGI.escapeHTML(bug_id)} (#{CGI.escapeHTML(e.message)})"
-      end
-    end
-    
-    render :layout => false
-  end
-  
-  def ajaxdescr
-    bug_ids = Bugzilla::Bug.str2bugIDs(params[:bugs])
-    
-    @bugs = []
-    bug_ids.each do |bug_id|
-      begin
-        @bugs << Bugzilla::Bug.load_from_id(bug_id.to_i)
-      rescue Exception => e
-      end
-    end  
-    
-    if @bugs.length == 1
-      @text = @bugs[0].summary
-      render :layout => false
-      return
-    end
-    
-    # Process 2 or more bugs
-    @atoms = []
-    @bugs.each do |bug|
-      matchdata = /([\w-]+)\/([\w-]+)(-([\w.]+))?/.match(bug.summary)
-      
-      unless matchdata.nil?
-        category = matchdata[1]
-        package = matchdata[2].gsub(/-+?$/, '')
-        
-        @atoms << "#{category}/#{package}"
-      end
-    end
-    
-    @atoms.uniq!
-    
-    if @atoms.length > 0
-      @text = @atoms.join(', ') + ": Multiple vulnerabilities"
-      render :layout => false
-      return
-    end
-
-    render :text => "(no suggestion available)", :layout => false
   end
 
   def background
