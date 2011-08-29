a0d78ec643f2ce5e6581944733a2f1de5ca268d6
Alex Legler <alex@a3li.li>
2011-08-29 16:09:23 +0200

    Add access tests for all admin controllers

--- test/functional/admin/users_controller_test.rb
+++ test/functional/admin/users_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should work for admins" do
+    @request.env['HTTP_AUTHORIZATION'] = basic_auth_creds('admin', GLSAMAKER_DEVEL_PASSWORD)
+    get :index
+    assert_response :success
+  end
+
+  test "should not grant access to regular users" do
+    @request.env['HTTP_AUTHORIZATION'] = basic_auth_creds('test', GLSAMAKER_DEVEL_PASSWORD)
+    get :index
+    assert_redirected_to :controller => '/index', :action => 'error', :type => 'access'
   end
 end
