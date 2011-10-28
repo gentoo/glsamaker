693ddc30eade1d4302b6062fb91a51b460e78836
Alex Legler <a3li@gentoo.org>
2011-10-28 23:23:57 +0200

    Let's call this GLSAMaker 2.0 \o/

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Glsamaker::Application.initialize!
 
-GLSAMAKER_VERSION="1.9-git"
+GLSAMAKER_VERSION="2.0"
 
 require 'digest/md5'
