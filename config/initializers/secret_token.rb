0a1f4b350b68ac65153ba809819277642346a812
Alex Legler <a3li@gentoo.org>
2011-10-28 23:36:06 +0200

    Drop secret_token.rb, set it in the local glsamaker config

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Glsamaker::Application.config.secret_token = '1cda0326aacd7d0e7c9e2feec468ff86f4ade897c17a1bfe9f7b820457c4bc1eaf2888fcfe4baa48fbd4e7fd32bd20a8abbe6b4d681c56c561390003aabc6979'
