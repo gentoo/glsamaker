7980ade366dc168f3a98bff66e775f0a0e597eef
Alex Legler <alex@a3li.li>
2013-10-13 20:25:01 +0200

    Fix precompiled asset path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Glsamaker::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( cvs.css jsui.js )
+  config.assets.precompile += %w( cve.css jsui.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
