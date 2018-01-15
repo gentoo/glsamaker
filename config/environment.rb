a56db29952801341417129cd7d1aeb9753ac8918
Thomas Deutschmann <whissi@gentoo.org>
2018-01-15 01:04:24 +0100

    Bump version.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application
 Rails.application.initialize!
 
-GLSAMAKER_VERSION = '2.0'
+GLSAMAKER_VERSION = '2.1.7'
 
 require 'digest/md5'
