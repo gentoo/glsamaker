a3577dd73287ac74b93dafc50dfe911ede6eda06
Robin H. Johnson <robbat2@gentoo.org>
2018-05-27 11:39:29 -0700

    Sync version tag
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application
 Rails.application.initialize!
 
-GLSAMAKER_VERSION = '2.1.9'
+GLSAMAKER_VERSION = '2.1.12'
 
 require 'digest/md5'
