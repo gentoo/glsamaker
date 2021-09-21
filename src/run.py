a618af44525f1a86f17527e8abbd82c2891bd8e8
John Helmert III <ajak@gentoo.org>
2021-09-20 21:55:22 -0500

    Add useless login form
    Signed-off-by: John Helmert III <ajak@gentoo.org>

--- src/run.py
+++ src/run.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
+import os
 
-from flask import current_app, Flask
+from flask import current_app, render_template, redirect, Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 @app.route("/")
