2e5b387fd4f00ad12237a15a23b00d578ef9f5a0
John Helmert III <ajak@gentoo.org>
2022-08-09 15:35:52 -0500

    glsamaker/views: use '0' instead of '*' for default slot
    Signed-off-by: John Helmert III <ajak@gentoo.org>

--- glsamaker/views.py
+++ glsamaker/views.py
@@ -152,7 +152,7 @@ def parse_atoms(request, range_type):
             # the string
             pkg_range = Affected.range_types[package.op]
             version = package.fullver
-            slot = package.slot or "*"
+            slot = package.slot or "0"
             ret.append(Affected(pn, version, pkg_range, arch, slot, range_type))
     return ret
 
