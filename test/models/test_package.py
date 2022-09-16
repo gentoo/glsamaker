fccd7da906a62074f8347b2e4db0d2f6aa087227
John Helmert III <ajak@gentoo.org>
2022-09-16 12:02:55 -0500

    test/models/test_package: test more cases
    Signed-off-by: John Helmert III <ajak@gentoo.org>

--- test/models/test_package.py
+++ test/models/test_package.py
@@ -1,4 +1,5 @@
 import pytest
+from pkgcore.ebuild import atom as atom_mod
 
 from glsamaker.models.package import Affected
 
@@ -9,8 +10,21 @@ class TestAffected:
         [
             ("dev-java/oracle-jre-bin", "dev-java/oracle-jre-bin"),
             ("<dev-java/openjdk-17.0.2_p8:17", "<dev-java/openjdk-17.0.2_p8:17"),
+            (
+                "<dev-java/oracle-jre-bin-1.8.0.202",
+                "<dev-java/oracle-jre-bin-1.8.0.202",
+            ),
         ],
     )
     def test_versioned_atom(self, a, b):
-        atom = Affected(a, None, None, "*", None, "vulnerable").versioned_atom()
+        a = atom_mod.atom(a)
+        atom = Affected(
+            str(a.unversioned_atom),
+            a.version,
+            # 'if' to properly handle unversioned atoms
+            Affected.range_types[a.op] if a.op else None,
+            "*",  # we're testing versioning, so use all arches
+            a.slot,
+            "vulnerable",
+        ).versioned_atom()
         assert atom == b
